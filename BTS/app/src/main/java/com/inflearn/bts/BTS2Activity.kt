b8abf64e64cd95e4ef6a55122e1391f99d690d68
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-09-26 21:27:39 +0900

    BTS 완성

---
+++ BTS/app/src/main/java/com/inflearn/bts/BTS2Activity.kt
@@ -0,0 +1,11 @@
+package com.inflearn.bts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BTS2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bts2)
+    }
+}
--- BTS/app/src/main/java/com/inflearn/bts/MainActivity.kt
+++ BTS/app/src/main/java/com/inflearn/bts/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.inflearn.bts
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image1 = findViewById<ImageView>(R.id.bts_image_1)
+        val image2 = findViewById<ImageView>(R.id.bts_image_2)
+        val image3 = findViewById<ImageView>(R.id.bts_image_3)
+        val image4 = findViewById<ImageView>(R.id.bts_image_4)
+        val image5 = findViewById<ImageView>(R.id.bts_image_5)
+        val image6 = findViewById<ImageView>(R.id.bts_image_6)
+        val image7 = findViewById<ImageView>(R.id.bts_image_7)
+
+        image1.setOnClickListener{
+            val intent = Intent(this,BTS1Activity::class.java)
+            startActivity(intent)
+        }
+        image2.setOnClickListener{
+            val intent = Intent(this,BTS2Activity::class.java)
+            startActivity(intent)
+        }
+        image3.setOnClickListener{
+            val intent = Intent(this,BTS3Activity::class.java)
+            startActivity(intent)
+        }
+        image4.setOnClickListener{
+            val intent = Intent(this,BTS4Activity::class.java)
+            startActivity(intent)
+        }
+        image5.setOnClickListener{
+            val intent = Intent(this,BTS5Activity::class.java)
+            startActivity(intent)
+        }
+        image6.setOnClickListener{
+            val intent = Intent(this,BTS6Activity::class.java)
+            startActivity(intent)
+        }
+        image7.setOnClickListener{
+            val intent = Intent(this,BTS7Activity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
