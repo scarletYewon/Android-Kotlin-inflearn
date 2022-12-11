25c47297073ee9d5a4c4b8f3953e6e07e5543e66
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-12-12 00:48:18 +0900

    trot

---
+++ trot/trot_list/app/src/main/java/com/bokchi/trot_list/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.bokchi.trot_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1. navigation 만들기
+
+        //2. Fragmenet 만들고 연결하기 네비게이션에
+            // 2.1 Fragment에서 버튼(이미지) 누르면은 이동하기
+
+        //3. 리사이클러뷰 만들기
+
+    }
+}
